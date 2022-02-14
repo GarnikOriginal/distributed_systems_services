36cedea54ae1d8931b1092cfb9eaf230e077f282
GarnikOriginal <gavid19912@gmail.com>
2022-02-14 10:27:59 +0500

    Remove old files

--- Makefile
+++
@@ -1,6 +0,0 @@
-build:
-	docker build . -t yolo_service
-update:
-	docker service update yolo_service_yolo_service --force
-run:
-	docker stack deploy -c swarmfile.yaml yolo_service
--- modules/decode_utils.py
+++
@@ -1,9 +0,0 @@
-import cv2
-import numpy as np
-
-
-def decode_image(binary):
-    array = np.fromstring(binary, np.uint8)
-    image = cv2.imdecode(array, cv2.IMREAD_ANYCOLOR)
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    return image
